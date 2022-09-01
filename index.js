913d0cf4ac917b0093f2d9cf55ee215297fbcad2
jonathon.biala@gmail.com <jonathon.biala@gmail.com>
2022-08-31 21:52:19 -0700

    logic for readme added and tested

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
 const fs = require("fs");
-const path = requre("path");
+const path = require("path");
 const inquirer = require("inquirer");
-const { default: Choice } = require("inquirer/lib/objects/choice");
-const createMarkdown = require("./utils/createMarkdown");
+const generateMarkdown = require("./utils/generateMarkdown");
 
 const questions = [
     {
         type: "input",
         name: "github",
-        message: "What is your github username?"
-    },
-    {
+        message: "What is your GitHub username?"
+    }, {
         type: "input",
         name: "email",
         message: "What is your email address?"
@@ -18,29 +16,29 @@ const questions = [
     {
         type: "input",
         name: "title",
-        message: "What is your project name?"
+        message: "What is your project's name?"
     },
     {
         type: "input",
         name: "description",
-        message: "please write a short description of your project"
+        message: "Please write a short description of your project:"
     },
     {
         type: "list",
         name: "license",
-        message: "What kind of license should your project have?"
-        choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BDS 3", "None"]
+        message: "What kind of license should your project have?",
+        choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "None"]
     },
     {
         type: "input",
         name: "installation",
         message: "What command should be run to install dependencies?",
-        devault: "npm i"
+        default: "npm i"
     },
     {
         type: "input",
         name: "test",
-        message: "What command should be used to run tests?",
+        message: "What command should be run to run tests?",
         default: "npm test"
     },
     {
@@ -51,7 +49,7 @@ const questions = [
     {
         type: "input",
         name: "contributing",
-        message: "What does the usert need to know about contributing to the repo?",
+        message: "What does the user need to know about contributing to the repo?",
     }
 ];
 
@@ -61,9 +59,9 @@ function writeToFile(fileName, data) {
 
 function init() {
     inquirer.prompt(questions)
-        .then((inquirerResponse) => {
+        .then((inquirerResponses) => {
             console.log("Generating README...");
-            writeToFile("README.md", createMarkdown({ ...inquirerResponses }));
+            writeToFile("README.md", generateMarkdown({ ...inquirerResponses }));
         })
 }
 
